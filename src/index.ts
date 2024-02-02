bd1a4abc4385fb709cad721644a52f771672cdc4
hayato.mochizuki <hayato.mochizuki@xincere.jp>
2024-02-02 22:37:17 +0900

    Formatted by prettier

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { parse } from '@progfay/scrapbox-parser';
-import { pageToMarkdown } from './pageToMarkdown';
-import * as fs from 'fs';
-import { escapePageName, downloadImages } from './util';
+import { parse } from "@progfay/scrapbox-parser";
+import { pageToMarkdown } from "./pageToMarkdown";
+import * as fs from "fs";
+import { escapePageName, downloadImages } from "./util";
 
 export async function run() {
-  console.info('Starting...');
-  process.stdin.setEncoding('utf8');
+  console.info("Starting...");
+  process.stdin.setEncoding("utf8");
 
-  const file = fs.readFileSync(process.argv[2], 'utf8');
+  const file = fs.readFileSync(process.argv[2], "utf8");
   const data = JSON.parse(file);
 
-  fs.mkdirSync('output', { recursive: true });
-  fs.mkdirSync('output/assets', { recursive: true });
+  fs.mkdirSync("output", { recursive: true });
+  fs.mkdirSync("output/assets", { recursive: true });
   for (const page of data.pages) {
-    const parsed = parse(page.lines.join('\n'), {
+    const parsed = parse(page.lines.join("\n"), {
       hasTitle: true,
     });
     // console.log(parsed);
@@ -22,8 +22,8 @@ export async function run() {
     fs.writeFileSync(`./output/${newTitle}.md`, markdown);
     console.log(`Created ${newTitle}.md`);
   }
-  console.info('Downloading images');
+  console.info("Downloading images");
   await downloadImages();
-};
+}
 
 run();
--- src/pageToMarkdown.ts
+++ src/pageToMarkdown.ts
@@ -1,141 +1,145 @@
-import type { Node, Page } from '@progfay/scrapbox-parser';
-import { escapePageName, enqueueGyazo, enqueueImage } from './util';
+import type { Node, Page } from "@progfay/scrapbox-parser";
+import { escapePageName, enqueueGyazo, enqueueImage } from "./util";
 
 function nodeToMarkdown(node: Node): string {
-  if (node.type === 'blank') {
+  if (node.type === "blank") {
     return node.text;
   }
-  if (node.type === 'code') {
+  if (node.type === "code") {
     return `\`${node.text}\``;
   }
-  if (node.type === 'commandLine') {
+  if (node.type === "commandLine") {
     return `\`${node.raw}\``;
   }
-  if (node.type === 'decoration') {
-    const prefix = '';
+  if (node.type === "decoration") {
+    const prefix = "";
     const tags: string[] = [];
     node.decos.forEach((deco) => {
-      if (deco[0] === '*') {
+      if (deco[0] === "*") {
         const decoLevel = parseInt(deco.substring(2), 10);
-        prefix.concat('#'.repeat(Math.max(6 - decoLevel + 1, 1)));
+        prefix.concat("#".repeat(Math.max(6 - decoLevel + 1, 1)));
         for (let i = 0; i < decoLevel - 6; i += 1) {
-          tags.push('strong');
+          tags.push("strong");
         }
-      } else if (deco === '-') {
-        tags.push('s');
-      } else if (deco === '_') {
-        tags.push('u');
-      } else if (deco === '/') {
-        tags.push('i');
+      } else if (deco === "-") {
+        tags.push("s");
+      } else if (deco === "_") {
+        tags.push("u");
+      } else if (deco === "/") {
+        tags.push("i");
       }
     });
-    return node.nodes.map((childNode) => nodeToMarkdown(childNode)).join('');
+    return node.nodes.map((childNode) => nodeToMarkdown(childNode)).join("");
   }
-  if (node.type === 'formula') {
+  if (node.type === "formula") {
     return `$${node.formula}$`;
   }
-  if (node.type === 'googleMap') {
+  if (node.type === "googleMap") {
     return node.url;
   }
-  if (node.type === 'hashTag') {
+  if (node.type === "hashTag") {
     return `[${node.raw}](${node.href})`;
   }
-  if (node.type === 'helpfeel') {
+  if (node.type === "helpfeel") {
     return `\`? ${node.text}\``;
   }
-  if (node.type === 'icon') {
+  if (node.type === "icon") {
     return `(${node.path})`;
   }
-  if (node.type === 'image') {
+  if (node.type === "image") {
     // FIXME
-    const src = '03_Attachments/' + (node.src.startsWith('https://gyazo.com') ? enqueueGyazo(node.src) : enqueueImage(node.src));
-    return `![[${src}]]`
+    const src =
+      "03_Attachments/" +
+      (node.src.startsWith("https://gyazo.com")
+        ? enqueueGyazo(node.src)
+        : enqueueImage(node.src));
+    return `![[${src}]]`;
   }
-  if (node.type === 'link') {
-    const content = node.content === '' ? node.href : node.content;
-    if (content === node.href && !content.startsWith('http')) {
+  if (node.type === "link") {
+    const content = node.content === "" ? node.href : node.content;
+    if (content === node.href && !content.startsWith("http")) {
       // internal wikilink
       const pageName = escapePageName(content);
-      return `[[${pageName}]]`
+      return `[[${pageName}]]`;
     }
     return `[${content}](${node.href})`;
   }
-  if (node.type === 'quote') {
+  if (node.type === "quote") {
     const content = node.nodes
       .map((childNode) => nodeToMarkdown(childNode))
-      .join('\n');
+      .join("\n");
     return `> ${content}`;
   }
-  if (node.type === 'strong') {
+  if (node.type === "strong") {
     const content = node.nodes
       .map((childNode) => nodeToMarkdown(childNode))
-      .join('\n');
+      .join("\n");
     return `<strong>${content}</strong>`;
   }
-  if (node.type === 'strongIcon') {
+  if (node.type === "strongIcon") {
     return `<strong>(${node.path})</strong>`;
   }
-  if (node.type === 'strongImage') {
+  if (node.type === "strongImage") {
     return `![${node.src}](${node.src})`;
   }
-  if (node.type === 'numberList') {
+  if (node.type === "numberList") {
     const content = node.nodes
       .map((childNode) => nodeToMarkdown(childNode))
-      .join('\n');
+      .join("\n");
     return `${node.number}. ${content}`;
   }
   // node.type === 'plain'
   return node.text;
-};
+}
 
 export function pageToMarkdown(page: Page): string {
   const lines = page.map((block) => {
-    if (block.type === 'title') {
-      return null;// `# ${block.text}`;
+    if (block.type === "title") {
+      return null; // `# ${block.text}`;
     }
-    if (block.type === 'codeBlock') {
+    if (block.type === "codeBlock") {
       const headline = `\`\`\`${block.fileName}`;
       if (block.indent === 0) {
         return `${headline}\n${block.content}\n\`\`\``;
       }
-      const indent = '\t'.repeat(block.indent - 1);
+      const indent = "\t".repeat(block.indent - 1);
       return [
         `${indent}- ${headline}`,
         `${block.content}\n\`\`\``
-          .split('\n')
+          .split("\n")
           .map((line) => `${indent}\t${line}`)
-          .join('\n'),
+          .join("\n"),
       ];
     }
-    if (block.type === 'table') {
+    if (block.type === "table") {
       const tableRows = block.cells.map((row) =>
         row.map((nodes) => {
-          `| ${nodes.map((node) => nodeToMarkdown(node)).join(' | ')} |`;
+          `| ${nodes.map((node) => nodeToMarkdown(node)).join(" | ")} |`;
         })
       );
       if (block.indent === 0) {
-        return `${block.fileName}\n${tableRows.join('\n')}`;
+        return `${block.fileName}\n${tableRows.join("\n")}`;
       }
-      const indent = '\t'.repeat(block.indent - 1);
+      const indent = "\t".repeat(block.indent - 1);
       return [
         `${indent}- ${block.fileName}`,
-        tableRows.map((line) => `${indent}\t${line}`).join('\n'),
+        tableRows.map((line) => `${indent}\t${line}`).join("\n"),
       ];
     }
     // block.type === 'line'
     if (block.nodes.length == 0) {
-      return '';
+      return "";
     }
-    const text = block.nodes.map((node) => nodeToMarkdown(node)).join('');
+    const text = block.nodes.map((node) => nodeToMarkdown(node)).join("");
     if (block.indent === 0) {
       return text;
     }
-    const indent = '\t'.repeat(block.indent - 1);
-    if (block.nodes[0].type === 'numberList') {
+    const indent = "\t".repeat(block.indent - 1);
+    if (block.nodes[0].type === "numberList") {
       return `${indent}${text}`;
     }
     return `${indent}- ${text}`;
   });
   lines.shift();
-  return lines.join('\n');
-};
+  return lines.join("\n");
+}
